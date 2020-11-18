b82366047cb5f93f3738b9434cda08742c1eea48
lazio <diniovskiy@gmail.com>
2020-11-18 10:32:43 +0200

    add comments

--- api/timeRequest.js
+++ api/timeRequest.js
@@ -1,29 +1,35 @@
-const { Log } = require('./models');
+const { Log } = require("./models");
 
 /* Функция принимает "Имя кеша" и "Время обработки запроса"
   И возвращает словарь ~ { timeDB: 620 } 
 **/
-function checkingCacheName(cacheName, timeQuery) {
-  if (cacheName == 'Node') {
-    return {timeNode: timeQuery};
-  }
-  if (cacheName == 'Redis') {
-    return {timeRedis: timeQuery};
-  }
-  if (cacheName == 'DB') {
-    return {timeDB: timeQuery};
-  }
-};
 
-/* Функция принимает "Имя кеша", "Время старта запроса", "Имя запроса" и расчитывает "Время обработки запроса", 
-**/
+// Не понятно зачем нужна эта функция. Она возвращает имя кеша и время которые у тебя и так есть.
+function checkingCacheName(cacheName, timeQuery) {
+  if (cacheName == "Node") {
+    return { timeNode: timeQuery };
+  }
+  if (cacheName == "Redis") {
+    return { timeRedis: timeQuery };
+  }
+  if (cacheName == "DB") {
+    return { timeDB: timeQuery };
+  }
+}
+
+// Лог не совсем верно работает.
+// Он не должен проверять есть ли уже такая запись в базе, но записывать каждое новое обращение за данными
+// и фиксировать сколько времени оно заняло
+
+/* Функция принимает "Имя кеша", "Время старта запроса", "Имя запроса" и расчитывает "Время обработки запроса",
+ **/
 async function timeRequest(cacheName, reqStart, requestName) {
   const timeQuery = new Date() - reqStart;
 
   const data = {
     requestName,
     cacheName,
-    timeQuery
+    timeQuery,
   };
 
   // Пытается найти по Имени запроса строку в БД
@@ -32,7 +38,7 @@ async function timeRequest(cacheName, reqStart, requestName) {
   /* Если все плохо фрмирует новую запись,
   логично предположить, что еcли это новая запись,
   то запрос идет из БД
-  **/ 
+  **/
   if (log === null) {
     console.log(`${data.requestName} - Not found`.red);
     Log.create({
@@ -42,18 +48,18 @@ async function timeRequest(cacheName, reqStart, requestName) {
       timeNode: 0,
     });
     console.log(`${data.requestName} - Saved`.blue);
-  }
-  
-  /* 
+  } else {
+    /* 
   Если есть что-то по "Имени запроса", то обновляет данные
   в соответствующих полях
   **/
-  else {
-    const checking = Object.keys(checkingCacheName(data.cacheName, data.timeQuery))[0]
-    log[checking] = data.timeQuery
+    const checking = Object.keys(
+      checkingCacheName(data.cacheName, data.timeQuery)
+    )[0];
+    log[checking] = data.timeQuery;
     await log.save();
     console.log(`${data.requestName} - Updated`.blue);
-  };
-};
+  }
+}
 
 module.exports = timeRequest;
--- api/views.js
+++ api/views.js
@@ -1,79 +1,79 @@
-const client = require('./redis');
-const timeRequest = require('./timeRequest');
-const { Log } = require('./models');
-
+const client = require("./redis");
+const timeRequest = require("./timeRequest");
+const { Log } = require("./models");
 
 const ttlLocal = 15;
 const ttlRedis = 30;
-let queryRedis = {fake:'fake'};
-let queryLocal = {fake:'fake'};
-
+let queryRedis = { fake: "fake" };
+let queryLocal = { fake: "fake" };
 
 function setCache(key, object) {
   queryLocal = { [key]: object };
   queryRedis = { [key]: object };
   client.set(key, JSON.stringify(object));
-};
+}
 
-
+// Подумай пожалуйста как убрать вложенные if-else, их очень сложно читать
+// Вот статья которая может помочь https://blog.codinghorror.com/flattening-arrow-code/
 function getOneData(obj) {
   return async (req, res) => {
     try {
       const pk = req.params.id;
-      let req_start = new Date();                        // Временная точка начала запроса
-      const originalUrl = req.originalUrl;               // Является ключом для кеша
+      let req_start = new Date(); // Временная точка начала запроса
+      const originalUrl = req.originalUrl; // Является ключом для кеша
 
-      if (originalUrl === Object.keys(queryLocal)[0]) {  // Если ключ есть в queryLocal то работает это блок
+      if (originalUrl === Object.keys(queryLocal)[0]) {
+        // Если ключ есть в queryLocal то работает это блок
         const object = queryLocal[originalUrl];
-        res.status(200).json(object);                    // Ответ в формате JSON
-        timeRequest('Node', req_start, originalUrl)      // Функция считает время работы блока
-      }
+        res.status(200).json(object); // Ответ в формате JSON
+        timeRequest("Node", req_start, originalUrl); // Функция считает время работы блока
+      } else {
+        if (originalUrl === Object.keys(queryRedis)[0]) {
+          // Если ключ есть в queryRedis то работает это блок
+          client.get(originalUrl, (err, object) => {
+            // Получить значение по ключу в Redis
+            if (err) console.log(err.message); // Выкинуть ошибку Redis в консоль
+            res.status(200).json(JSON.parse(object)); // Ответ в формате JSON
+            timeRequest("Redis", req_start, originalUrl); // Функция считает время работы блока
+          });
+        } else {
+          const object = await obj.findByPk(+pk); // Если queryLocal & queryRedis пусты работает этот блок
+          res.status(200).json(object); // Ответ в формате JSON
+          timeRequest("DB", req_start, originalUrl); // Функция считает время работы блока
 
-      else {
-        if (originalUrl === Object.keys(queryRedis)[0]) { // Если ключ есть в queryRedis то работает это блок
-          client.get(originalUrl, (err, object) => {      // Получить значение по ключу в Redis
-            if (err) console.log(err.message);            // Выкинуть ошибку Redis в консоль
-            res.status(200).json(JSON.parse(object));     // Ответ в формате JSON
-            timeRequest('Redis', req_start, originalUrl); // Функция считает время работы блока
-          })
-        }
-
-        else {
-          const object = await obj.findByPk(+pk);         // Если queryLocal & queryRedis пусты работает этот блок
-          res.status(200).json(object);                   // Ответ в формате JSON
-          timeRequest('DB', req_start, originalUrl)       // Функция считает время работы блока
-
-          setTimeout(() => {                              // Сброс queryRedis, Redis 
+          setTimeout(() => {
+            // Сброс queryRedis, Redis
             client.del(originalUrl, (err, reply) => {
               if (err) {
                 console.log(err.message);
-              }
-              else {
-                queryRedis = { fake: 'fake' }
+              } else {
+                queryRedis = { fake: "fake" };
                 console.log("Redis Del", reply);
               }
-            }
-          )}, 1000 * ttlRedis);
+            });
+          }, 1000 * ttlRedis);
 
-          setTimeout(() => {                              // Сброс queryLocal
-            queryLocal = {fake:'fake'}
+          setTimeout(() => {
+            // Сброс queryLocal
+            queryLocal = { fake: "fake" };
           }, 1000 * ttlLocal);
 
-          setCache(originalUrl, object);                  // Устанавливает значения для queryLocal & queryRedis
+          setCache(originalUrl, object); // Устанавливает значения для queryLocal & queryRedis
         }
       }
-
     } catch (error) {
       res.status(400).json({ msg: "400 Bad Request" });
     }
   };
 }
 
-
 function getAllData(obj) {
   return async (req, res) => {
     try {
-      const object = await obj.findAndCountAll({ limit: req.query.limit, offset: req.skip })
+      const object = await obj.findAndCountAll({
+        limit: req.query.limit,
+        offset: req.skip,
+      });
       res.status(200).json(object);
     } catch (error) {
       res.status(400).json({ msg: "400 Bad Request" });
